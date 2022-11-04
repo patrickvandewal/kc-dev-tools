68e50d892cbfe2183b531acaefb29c4e8f3d5de5
Patrick van de Wal <p.vandewal@Live.nl>
2022-11-04 13:40:23 +0100

    Update DevToolsServiceProvider.php

--- src/Providers/DevToolsServiceProvider.php
+++ src/Providers/DevToolsServiceProvider.php
@@ -8,6 +8,6 @@ class DevToolsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../Http/routes.php');
+        $this->loadRoutesFrom(__DIR__.'/../Http/Routes.php');
     }
 }
