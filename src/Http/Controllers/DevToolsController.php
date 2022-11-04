78bcac52cd7104a45662c98da0ff7375b8f9bcd4
Patrick van de Wal <p.vandewal@Live.nl>
2022-11-04 15:56:39 +0100

    main

--- src/Http/Controllers/DevToolsController.php
+++ src/Http/Controllers/DevToolsController.php
@@ -2,16 +2,48 @@
 
 namespace KingsCode\DevTools\Http\Controllers;
 
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Support\Facades\Redirect;
+use Symfony\Component\Console\Command\Command;
 use Illuminate\Routing\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 class DevToolsController extends Controller
 {
+    private const DIRECTORY = 'App\Console\Commands';
+
     /**
      * Default view
+     *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public static function overview()
+    public function overview()
     {
-        return 'hi';
+        $commands = collect(resolve(Kernel::class)->all())->filter(function (Command $command) {
+            return str_contains(get_class($command), self::DIRECTORY);
+        })->map(function (Command $command) {
+            return [
+                'name'  => $command->getName(),
+                'value' => $command,
+            ];
+        })->push([
+            'name' => '--- Select a command ---',
+            'value' => null,
+        ])->sortBy('name');
+
+        return view('vendor.kc-dev-tools.overview', [
+            'commands' => $commands->values()->toArray(),
+        ]);
+    }
+
+    public function process(Request $request) {
+
+        $commandName = $request->input('command-name');
+        $args = $request->input('arguments') ?? [];
+        Artisan::call($commandName, $args);
+
+        return Redirect::to($request->session()->previousUrl());
     }
 }
--- src/Http/Routes.php
+++ src/Http/Routes.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'dev-tools' , 'middleware' => ['web']], static function () {
-    Route::get('overview', ['uses' => 'DevToolsController@overview']);
-    Route::post('overview', ['uses' => 'DevToolsController@overview']);
-    Route::post('process', ['uses' => 'DevToolsController@process']);
+Route::group(['prefix' => 'dev-tools', 'middleware' => ['web']], static function () {
+    Route::get('overview', ['uses' => 'KingsCode\\DevTools\\Http\\Controllers\\DevToolsController@overview']);
+    Route::post('process', ['uses' => 'KingsCode\\DevTools\\Http\\Controllers\\DevToolsController@process']);
 });
