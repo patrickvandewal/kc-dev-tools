d7241658d0c2763967187b768735dbf6c259d500
Patrick van de Wal <p.vandewal@Live.nl>
2022-11-09 15:59:27 +0100

    Update DevToolsServiceProvider.php

--- src/Providers/DevToolsServiceProvider.php
+++ src/Providers/DevToolsServiceProvider.php
@@ -9,5 +9,11 @@ class DevToolsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/../Http/Routes.php');
+
+        $this->loadViewsFrom(__DIR__.'/../../resources/', 'kc-dev-tools');
+
+        $this->publishes([
+            __DIR__.'/views/' => base_path('resources/views/vendor/kc-dev-tools'),
+        ]);
     }
 }
