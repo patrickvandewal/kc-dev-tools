84b01ab2e923926ee1d56413d45ab0ba39f05399
Patrick van de Wal <p.vandewal@Live.nl>
2022-11-09 17:56:04 +0100

    Update DevToolsController.php

--- src/Http/Controllers/DevToolsController.php
+++ src/Http/Controllers/DevToolsController.php
@@ -1,11 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace KingsCode\DevTools\Http\Controllers;
 
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Support\Facades\Redirect;
 use Symfony\Component\Console\Command\Command;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
@@ -14,6 +17,11 @@ class DevToolsController extends Controller
 {
     private const DIRECTORY = 'App\Console\Commands';
 
+    private const NAME_INCLUSIONS = [
+        'optimize:clear',
+        'cache:clear'
+    ];
+
     /**
      * Default view
      *
@@ -21,8 +29,15 @@ class DevToolsController extends Controller
      */
     public function overview()
     {
-        $commands = collect(resolve(Kernel::class)->all())->filter(function (Command $command) {
-            return str_contains(get_class($command), self::DIRECTORY);
+        $nameInclusions = collect(self::NAME_INCLUSIONS);
+
+        $commands = collect(resolve(Kernel::class)->all())->filter(function (Command $command) use ($nameInclusions) {
+            if ($nameInclusions->contains($command->getName()) ||
+                str_contains(get_class($command), self::DIRECTORY)) {
+                return true;
+            }
+
+            return false;
         })->map(function (Command $command) {
             return [
                 'name'  => $command->getName(),
@@ -33,13 +48,13 @@ class DevToolsController extends Controller
             'value' => null,
         ])->sortBy('name');
 
-        return view('kc-dev-tools::overview', [
-            'commands' => $commands->values()->toArray(),
-        ]);
+        return view('kc-dev-tools::overview', ['commands' => $commands->values()->toArray(),]);
     }
 
-    public function process(Request $request)
-    {
+    public
+    function process(
+        Request $request
+    ) {
         $commandName = $request->input('command-name');
         $args = $request->input('arguments') ?? [];
         Artisan::call($commandName, $args);
