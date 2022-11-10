3ea6ba25dad75e15b3f20b0b9d01b1f7fed2c46c
Patrick van de Wal <p.vandewal@Live.nl>
2022-11-10 11:46:41 +0100

    added password reset

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -4,6 +4,51 @@
 
 <html lang="en">
 <title>KingsCode Dev Tools</title>
+<style>
+
+body, form, input, option, select, .console {
+  font-family: Verdana;
+  font-size: 16px;
+}
+
+.console {
+  width: 575px;
+  height: 750px;
+  box-sizing: border-box;
+}
+
+.console .consolebody {
+  box-sizing: border-box;
+  padding: 20px;
+  overflow: scroll;
+  overflow-x: hidden;
+  background-color: #000;
+  color: #63de00;
+}
+
+.column {
+  float: left;
+  width: 50%;
+}
+
+.row:after {
+  display: table;
+  clear:   both;
+}
+
+.message {
+  color: orange;
+}
+
+#description {
+  width: 500px;
+}
+
+.content {
+  padding: 1%;
+}
+
+</style>
 <script>
 
 function commandSelected() {
@@ -12,38 +57,83 @@ function commandSelected() {
 </script>
 
 <body>
+<div class="content">
+	<div class="row">
+		<div class="column">
 
-<form id="selector" action="" method="get">
-	<select name="command-index" onchange="commandSelected()">
-		@foreach($commands as $index => $command)
-			{{ $isSelected = $selectedIndex === $index ? 'selected' : '' }}
-			<option {{$isSelected}} value={{ $index }}>{{ $command['name'] }}</option>
-		@endforeach
-	</select>
-</form>
+			<h1>Commands:</h1>
 
-@if(data_get($_GET, 'command-index'))
+			<form id="selector" action="" method="get">
+				<select name="command-index" onchange="commandSelected()">
+					@foreach($commands as $index => $command)
+						{{ $isSelected = $selectedIndex === $index ? 'selected' : '' }}
+						<option {{$isSelected}} value={{ $index }}>{{ $command['name'] }}</option>
+					@endforeach
+				</select>
+			</form>
 
-	<form action="/dev-tools/process" method="post">
+			@if(data_get($_GET, 'command-index'))
 
-		@php
-			$command = $commands[$selectedIndex]['value'];
-		@endphp
+				<form action="/dev-tools/process" method="post">
 
-		<div style="width: 750px;">{{$command->getDescription()}}</div>
-		<p>
-			@php
-				foreach($command->getDefinition()->getArguments() as $index => $argument) {
-					echo '<input name="arguments['.$argument->getName().']" placeholder='.$argument->getName().' /><br />';
-				}
-			@endphp
-		</p>
-		<p>
-			<input type="hidden" name="command-name" value="{{ $command->getName() }}">
-			<input type="hidden" name="command-index" value="{{ $selectedIndex }}">
-			<input type="submit" value="Execute"/>
-		</p>
-	</form>
-@endif
+					@php
+						$command = $commands[$selectedIndex]['value'];
+					@endphp
+
+					<div class="description">{{$command->getDescription()}}</div>
+					<p>
+						@php
+							foreach($command->getDefinition()->getArguments() as $index => $argument) {
+								echo '<input name="arguments['.$argument->getName().']" placeholder='.$argument->getName().' /><br />';
+							}
+						@endphp
+					</p>
+					<p>
+						<input type="hidden" name="command-name" value="{{ $command->getName() }}">
+						<input type="hidden" name="command-index" value="{{ $selectedIndex }}">
+						<input type="submit" value="Execute"/>
+					</p>
+				</form>
+			@endif
+
+			@if(data_get($_GET, 'command.message') !== null)
+				Message:
+				<div class="message"> {{ data_get($_GET, 'command.message') }} </div>
+			@endif
+
+			@if(data_get($_GET, 'command.output'))
+				<div class="console">
+					<div class="consolebody">
+						<p>{!! data_get($_GET, 'command.output') !!}</p>
+					</div>
+				</div>
+			@endif
+
+		</div>
+		<div class="column">
+
+			<h1>Reset Password:</h1>
+
+			<form action="/dev-tools/updatePassword" method="post">
+				<p>
+					<input name="password-reset[email]" placeholder="E-mail" type="text">
+				</p>
+				<p>
+					<input name="password-reset[password]" placeholder="New Password" type="password">
+				</p>
+				<p>
+					<input type="submit" value="Reset password"/>
+				</p>
+				@if(data_get($_GET, 'password-reset.message') !== null)
+					<p>
+					<div class="message">
+						{{ data_get($_GET, 'password-reset.message') }}
+					</div>
+					</p>
+				@endif
+			</form>
+		</div>
+	</div>
+</div>
 </body>
 </html>
--- src/Http/Controllers/DevToolsController.php
+++ src/Http/Controllers/DevToolsController.php
@@ -4,14 +4,19 @@ declare(strict_types=1);
 
 namespace KingsCode\DevTools\Http\Controllers;
 
+use Dompdf\Exception;
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Redirect;
+use Pushok\Response;
 use Symfony\Component\Console\Command\Command;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 class DevToolsController extends Controller
 {
@@ -19,7 +24,7 @@ class DevToolsController extends Controller
 
     private const NAME_INCLUSIONS = [
         'optimize:clear',
-        'cache:clear'
+        'cache:clear',
     ];
 
     /**
@@ -51,14 +56,45 @@ class DevToolsController extends Controller
         return view('kc-dev-tools::overview', ['commands' => $commands->values()->toArray(),]);
     }
 
-    public
-    function process(
-        Request $request
-    ) {
+    public function process(Request $request): RedirectResponse
+    {
         $commandName = $request->input('command-name');
         $args = $request->input('arguments') ?? [];
         Artisan::call($commandName, $args);
 
-        return Redirect::to($request->session()->previousUrl());
+        $parsedMessage = Artisan::output();
+        $parsedMessage = collect(explode('.', $parsedMessage))->join('.<br />');
+
+        return redirect()->action(
+            [DevToolsController::class, 'overview'],
+
+            array_merge($request->post(),
+            [
+                'command[output]' => $parsedMessage,
+                'command[message' => 'Command executed',
+            ]),
+        );
+    }
+
+    public function updatePassword(Request $request): RedirectResponse
+    {
+        try {
+            $email = $request->input('password-reset.email');
+            $password = $request->input('password-reset.password');
+
+            $user = \App\Models\User::query()->where('email', $email)->first();
+            if ($user === null) {
+                throw new Exception('User with the provided e-mail has not been found');
+            }
+
+            $message = 'Password updated';
+            $user->update(['password' => bcrypt($password)]);
+        } catch (\Exception $ex) {
+            $message = $ex->getMessage();
+        }
+
+        return redirect()->action(
+            [DevToolsController::class, 'overview'], ['password-reset[message]' => $message],
+        );
     }
 }
